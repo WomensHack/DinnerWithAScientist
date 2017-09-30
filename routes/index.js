c48ec214d7158d05da03da8d0c5000799192c09d
victoria tapia <victoria_tapia@discovery.com>
2017-09-30 12:38:10 -0700

    run static html on server

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// app.use(express.static(__dirname + '/public'));
 
 module.exports = router;
