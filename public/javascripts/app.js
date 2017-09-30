d8b67997244319f88427a5984ecfbc26f08b6898
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-30 14:20:56 -0700

    mentor and mentee js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#fullpage').fullpage();
 });
 console.log('hi');
+
+function setUserType(data) {
+    if (data == "mentee") {
+        var userType = document.getElementById("userType");
+        userType.innerHTML = "mentee";
+    } else if (data == "mentor") {
+        var userType = document.getElementById("userType");
+        userType.innerHTML = "mentor";
+    }
+
+
+
+}
